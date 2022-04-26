7b621014d9488390632a20ef059cb176e1750210
Naufal Azzmi <naufal.chroome@gmail.com>
2022-04-27 05:35:30 +0800

    Update SoalHandler.cs

--- Assets/Scripts/SoalHandler.cs
+++ Assets/Scripts/SoalHandler.cs
@@ -14,7 +14,9 @@ public class SoalHandler : MonoBehaviour
 
     public int jumlahBlok;
     public int jumlahOperand;
+    public List<float> ListBilangan;
 
+<<<<<<< Updated upstream
     
     public List<float> ListBilangan;
 
@@ -23,6 +25,10 @@ public class SoalHandler : MonoBehaviour
     public float curDifficulty = 0f;
 
     private Generator gen;
+=======
+    private Generator gen;
+    
+>>>>>>> Stashed changes
     
     private void Start()
     {
@@ -30,9 +36,14 @@ public class SoalHandler : MonoBehaviour
         setJumlahOperand();
 
         gen = this.gameObject.GetComponent<Generator>();
-        
-       // Debug.Log(gen);
-        buatSoal();
+
+        // Debug.Log(gen);
+        List<Bilangan> bilanganObj =  BuatSoal();
+        float difficultyIndex = getDifficulty();
+        Debug.Log(difficultyIndex);
+        GenerateSoal(bilanganObj);
+        // TODO: Check difficultynya
+        // kalo bener yaudah generate soalnya
     }
 
     private void setJumlahOperand()
@@ -55,10 +66,10 @@ public class SoalHandler : MonoBehaviour
         }
     }
 
-    public void buatSoal()
+    public List<Bilangan> BuatSoal()
     {
         List<Bilangan> bilangan = new List<Bilangan>();
-        
+
         for (int i = 0; i < jumlahTambah; i++)
         {
             float randNum = Random.Range(1, 10); // disini randomnya TODO mu
@@ -86,7 +97,7 @@ public class SoalHandler : MonoBehaviour
             {
                 bilangan.Add(gen.newBilangan("-" + randNum.ToString(), "*"));
             }
-            
+
         }
         for (int i = 0; i < jumlahBagi; i++) // PEMBAGIAN INI TRICKY SEKALI, jadi 0.00002 bisa
         {
@@ -104,6 +115,7 @@ public class SoalHandler : MonoBehaviour
 
         }
 
+<<<<<<< Updated upstream
         Node root = gen.newNode(bilangan, null, null);
         gen.generateChildrenNodes(root);
 
@@ -133,6 +145,9 @@ public class SoalHandler : MonoBehaviour
 
         gen.generateTarget(targetNode);
         gen.printSolution(targetNode);
+=======
+        return bilangan;
+>>>>>>> Stashed changes
     }
 
     private float getCombination(int jumlahBlok)
@@ -143,7 +158,7 @@ public class SoalHandler : MonoBehaviour
     private int factorial(int a)
     {
         int fact = 1;
-        for(int x=1; x <= a; x++)
+        for (int x = 1; x <= a; x++)
         {
             fact *= x;
         }
@@ -151,10 +166,10 @@ public class SoalHandler : MonoBehaviour
         return fact;
     }
 
-    public void getDifficulty()
+    private float getDifficulty()
     {
         float totalSum = 0;
-        
+
         foreach (float c in ListBilangan)
         {
             totalSum += c;
@@ -169,6 +184,7 @@ public class SoalHandler : MonoBehaviour
         float z3 = 1 - ((maxi - mini) / 9f);
         float z4 = jumlahOperand / 4f;
 
+<<<<<<< Updated upstream
         
         Debug.Log((z1)+"-"+z2+"-"+z3+"-"+z4);
         Debug.Log(((z1)+z2+z3+z4)/4);
@@ -179,5 +195,53 @@ public class SoalHandler : MonoBehaviour
     public void getScore()
     {
         // sum bilangan * 10 + second waktu (sisa) + 500 per operator * jumlah depth <= kalo dia bener
+=======
+
+        //float be = Mathf.Log10(totalSum) / ListBilangan.Count;
+        //float es = 3 / 8;//searchdepth
+        //float oh = 3 / 4;//operator
+
+
+
+        //float je = (Mathf.Log10(jumlahOperand) * totalSum) / jumlahBlok;
+        //float ka = (jumlahKali/ jumlahBlok) * (sumKali/totalSum); //ini baru untuk kali saja
+        //float detphSearch = 3 / 8;
+
+        Debug.Log((z1 / 28) + "-" + z2 + "-" + z3 + "-" + z4);
+        return (((z1 / 28) + z2 + z3 + z4) / 4); //KOK BISA SALAH COK?
+
+        //  Debug.Log(be + es + oh);
     }
+
+    public void GenerateSoal(List<Bilangan> bilangan)
+    {
+        
+        Node root = gen.newNode(bilangan, null, null);
+        gen.generateChildrenNodes(root);
+
+        generateTarget(root);
+        
+        StartCoroutine(gen.instantiateforSec(bilangan, 0.7f));
+    }
+
+    public void generateTarget(Node root)
+    {
+        Node targetNode = root;
+        int totalCombination = root.listBilangan.Count; // 4,3,2,1.
+
+        // TODO MU: Sebaiknya km atur depth dan check apa bener semuanya ga contain root bilangan?
+        for (int i = 0; i < totalCombination - 1; i++)
+        {
+
+            int targetchild = Random.Range(0, targetNode.child.Count);
+            targetNode = targetNode.child[targetchild];
+        }
+
+
+        gen.generateTarget(targetNode);
+        gen.printSolution(targetNode);
+>>>>>>> Stashed changes
+    }
+
+    
 }
